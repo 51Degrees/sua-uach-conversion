29c25999d671193fa312dbcbb0c22fff6a708bf1
Eugene Dorfman <eugene.dorfman@gmail.com>
2023-04-07 15:10:34 +0200

    imporved example_uach_sua

--- example_uach_sua.js
+++ example_uach_sua.js
@@ -1,15 +1,15 @@
 const convertUACHtoSUA = require("./src/convertUACHtoSUA.js");
 
 const uach = {
-  "sec-ch-ua-arch": '"x86"',
-  "sec-ch-ua-bitness": '"64"',
-  "sec-ch-ua-full-version-list":
-    '"Google Chrome";v="111.0.5563.146", "Not(A:Brand";v="8.0.0.0", "Chromium";v="111.0.5563.146"',
-  "sec-ch-ua-mobile": "?0",
-  "sec-ch-ua-model": "",
-  "sec-ch-ua-platform": '"macOS"',
-  "sec-ch-ua-platform-version": '"13.3.0"',
-};
+  'Sec-CH-UA': '"Google Chrome";v="111.0.5563.146", "Not(A:Brand";v="8.0.0.0", "Chromium";v="111.0.5563.146"',
+  'Sec-CH-UA-Full-Version-List': '"Google Chrome";v="111.0.5563.146", "Not(A:Brand";v="8.0.0.0", "Chromium";v="111.0.5563.146"',
+  'Sec-CH-UA-Platform': '"macOS"',
+  'Sec-CH-UA-Platform-Version': '"13.3"',
+  'Sec-CH-UA-Mobile': '?0',
+  'Sec-CH-UA-Arch': '"x86"',
+  'Sec-CH-UA-Bitness': '"64"'
+}
+;
 
 let sua = convertUACHtoSUA(uach);
-console.log(sua);
+console.log(JSON.stringify(sua, null, 2));
