9dfb00a4b7ebb1c668803ac46f1155ab2b34ec2f
romansertsov <roman.sertsov@postindustria.com>
2023-04-10 14:55:04 +0300

    Some refactor

--- src/scripts/converters/convertSUAtoUACH.js
+++ src/scripts/converters/convertSUAtoUACH.js
@@ -1,40 +1,38 @@
 const convertSUAtoUACH = (SUAObj) => {
-
-  if(SUAObj === null || SUAObj === undefined) throw new Error('Headers param cannot be empty.');
-  if(Object.keys(SUAObj).length === 0) return {};
+  if (SUAObj === null || SUAObj === undefined)
+    throw new Error("Headers param cannot be empty.");
+  if (Object.keys(SUAObj).length === 0) return {};
 
   const mappedHeaders = {};
 
   const { browsers, platform, mobile, architecture, bitness, model } = SUAObj;
 
-  if (browsers && browsers.length) {
+  if (browsers && browsers.length > 0) {
     const brands = browsers
-      .map((browser) => `"${Object.keys(browser)[0]}";v="${Object.values(browser)[0]}"`)
+      .map((browser) => {
+        return `"${browser.brand}";v="${browser.version.join(".")}"`;
+      })
       .join(", ");
     mappedHeaders["Sec-CH-UA"] = brands;
     mappedHeaders["Sec-CH-UA-Full-Version-List"] = brands;
   }
 
   if (platform) {
-    const platformKeys = Object.keys(platform);
-    if (platformKeys.length > 0) {
-      mappedHeaders["Sec-CH-UA-Platform"] = platformKeys[0];
-      mappedHeaders["Sec-CH-UA-Platform-Version"] = `${platform[platformKeys[0]]}`;
-    }
+    mappedHeaders["Sec-CH-UA-Platform"] = `"${platform.brand}"`;
+    mappedHeaders["Sec-CH-UA-Platform-Version"] = `"${platform.version.join(
+      "."
+    )}"`;
   }
 
   mappedHeaders["Sec-CH-UA-Mobile"] = mobile ? "?1" : "?0";
 
-  if (architecture)
-    mappedHeaders["Sec-CH-UA-Arch"] = architecture;
+  if (architecture) mappedHeaders["Sec-CH-UA-Arch"] = `"${architecture}"`;
 
-  if (bitness)
-    mappedHeaders["Sec-CH-UA-Bitness"] = bitness;
+  if (bitness) mappedHeaders["Sec-CH-UA-Bitness"] = `"${bitness}"`;
 
-  if (model)
-    mappedHeaders["Sec-CH-UA-Model"] = model;
+  if (model) mappedHeaders["Sec-CH-UA-Model"] = `"${model}"`;
 
   return mappedHeaders;
 };
 
-export default convertSUAtoUACH
+export default convertSUAtoUACH;
--- src/scripts/converters/convertUACHtoSUA.js
+++ src/scripts/converters/convertUACHtoSUA.js
@@ -1,37 +1,51 @@
-const convertUACHtoSUA = (headers) =>  {
+const convertUACHtoSUA = (headers) => {
+  if (typeof headers != "object")
+    throw new Error("Header must be an valid header map.");
+  if (headers === null || headers === undefined)
+    throw new Error("Headers param cannot be empty.");
+  if (Object.keys(headers).length === 0) return {};
 
-  if(headers === null || headers === undefined) throw new Error('Headers param cannot be empty.');
-  if(Object.keys(headers).length === 0) return {};
+  const SUAObject = {};
 
-  const headerMap = Object.entries(headers).reduce((accumulator, [key, value]) => {
-    accumulator[key.toLowerCase()] = value;
-    return accumulator;
-  }, {});
+  const headerMap = Object.entries(headers).reduce(
+    (accumulator, [key, value]) => {
+      accumulator[key.toLowerCase()] = value;
+      return accumulator;
+    },
+    {}
+  );
 
-  const matches = headerMap['sec-ch-ua'].match(/"([^"]+)";v="([^"]+)"/g);
-  const browsers = matches.map(match => {
+  const matches = headerMap["sec-ch-ua-full-version-list"].match(
+    /"([^"]+)";v="([^"]+)"/g
+  );
+
+  SUAObject["browsers"] = matches.map((match) => {
     const [, name, version] = match.match(/"([^"]+)";v="([^"]+)"/);
-    return { [name]: version };
+    return {
+      brand: name,
+      version: version.split("."),
+    };
   });
 
-  const platform = {
-    [headerMap['sec-ch-ua-platform']]: headerMap['sec-ch-ua-platform-version']
-  };
+  if (headerMap["sec-ch-ua-platform"])
+    SUAObject["platform"] = {
+      brand: headerMap["sec-ch-ua-platform"].replace(/"/g, ""),
+      version: headerMap["sec-ch-ua-platform-version"]
+        .split(".")
+        .map((v) => v.replace(/"/g, "")),
+    };
 
+  if (headerMap["sec-ch-ua-mobile"])
+    SUAObject["mobile"] = headerMap["sec-ch-ua-mobile"] === "?1" ? 1 : 0;
+  if (headerMap["sec-ch-ua-arch"])
+    SUAObject["architecture"] = headerMap["sec-ch-ua-arch"].replace(/"/g, "");
+  if (headerMap["sec-ch-ua-bitness"])
+    SUAObject["bitness"] = headerMap["sec-ch-ua-bitness"].replace(/"/g, "");
 
-  const mobile = headerMap['sec-ch-ua-mobile'] === '?1' ? '1' : '0';
-  const architecture = headerMap['sec-ch-ua-arch'];
-  const bitness = headerMap['sec-ch-ua-bitness'];
-  const model = headerMap['sec-ch-ua-model'];
+  if (headerMap["sec-ch-ua-model"])
+    SUAObject["model"] = headerMap["sec-ch-ua-model"];
 
-  return {
-    browsers,
-    platform,
-    mobile,
-    architecture,
-    bitness,
-    model
-  };
-}
+  return SUAObject;
+};
 
-export default convertUACHtoSUA
+export default convertUACHtoSUA;
